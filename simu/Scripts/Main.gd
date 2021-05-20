c22d0c3d11c3fc1a563d76184dce5c4147684a32
Victor <victor.bayona@ecl18.ec-lyon.fr>
2021-05-20 15:43:13 +0200

    fixed load of scenario from path in arguments

--- simu/Scripts/Main.gd
+++ simu/Scripts/Main.gd
@@ -17,29 +17,9 @@ var robots_list
 
 var packages_nb : int = 0
 
-#var processes_list
-#each element of the array is an array of integers 
-#corresponding to the machines possible for the given process
-#example : if process no.3 can be done by machines 4 or 5, 
-#		   then the element at index 3 can be [4,5] (or [5,4])
-
 var possible_tasks
 
 func _ready():	
-	#initialization
-#	var test_templates = [ [[0,10],[1,5]], [[0,1],[1,8],[2,6]], [[2,3],[1,9]], [[2,7],[0,12],[5,4]] ]
-#	var test_processes = [[Process.new(0,0), Process.new(1,0), Process.new(2,0)], [Process.new(2,0), Process.new(3,0), Process.new(4,0), Process.new(5,0)]]
-#	var machine_nb := 0
-#	for machine in get_tree().get_nodes_in_group("machines"):
-#		if machine.is_in_group("input_machines"):
-#			machine.packages_templates = test_templates
-#			machine.create_time = 5.0
-#		elif machine.is_in_group("output_machines"):
-#			pass
-#		else:
-#			machine.processes.processes = test_processes[machine_nb]
-#			machine_nb += 1
-
 
 	#values of arguments
 	
@@ -52,7 +32,9 @@ func _ready():
 	load_scenario(scenario_file)
 	
 	var pickup_radius = float(get_arg(arguments,"--pickup-radius",100 ))
-	_Robot.get_node("RayCast2D").set_cast_to(Vector2.RIGHT*pickup_radius)
+	var robots_list = get_tree().get_nodes_in_group("robots")
+	for robot in robots_list:
+		robot.get_node("RayCast2D").set_cast_to(Vector2.RIGHT*pickup_radius)
 	
 	
 	var log_name = get_arg(arguments,"--log", "")
@@ -91,11 +73,31 @@ func remove_package(package : Node):
 	
 	
 func load_scenario(file_path : String):
+	var absolute_scenario_path
 	
-	
+	if "res://" in file_path:
+		#in that case no need to convert path to absolute path
+		absolute_scenario_path = file_path
+	else:
+		#convert path to absolute path
+		var separated_path
+		if '/' in file_path:
+			separated_path = file_path.split('/')
+		else:
+			separated_path = file_path.split('\\')
+		var file_name = separated_path[-1]
+		separated_path.remove(separated_path.size()-1)
+		var directory_path = separated_path.join("/")
+		
+		var dir = Directory.new()
+		dir.open(OS.get_executable_path().get_base_dir())
+		dir.change_dir(directory_path)
+		
+		absolute_scenario_path = dir.get_current_dir() + "/" + file_name
+		
 	#load scenario file
 	var file = File.new()
-	var open_error = file.open(file_path, File.READ) 
+	var open_error = file.open(absolute_scenario_path, File.READ) 
 	if open_error:
 		Logger.log_error("Error opening the scenario file (Error code %s)" % open_error)
 		return
