9334bbcde04bc45e0f384c619726872c0f7813d8
Victor Bayona <victor.bayona@ecl18.ec-lyon.fr>
2021-06-21 11:03:10 +0200

    changed format of action messages

--- clients/python_client/ActionClientManager.py
+++ clients/python_client/ActionClientManager.py
@@ -16,9 +16,9 @@ class ActionClientManager():
 			#format of command is a list containing name of command and arguments, for example ['navigate_to','robot1',50,100]
 			#the functions wait until the first response from the server, and then returns the ID attributed to the action
 			temp_id = str(uuid.uuid4())
-			data_to_send = {'type': 'robot_command'}
-			data_to_send['data'] = command
-			data_to_send['temp_id'] = temp_id		
+			data_to_send = {'type': 'robot_command', 'data' : {}}
+			data_to_send['data']['command_info'] = command
+			data_to_send['data']['temp_id'] = temp_id		
 
 			new_action = ActionClient(self, temp_id)
 			self.actions[temp_id] = new_action
@@ -46,8 +46,8 @@ class ActionClientManager():
 
 
 	def receive_response(self, response_message : Dict):
-		temp_id = response_message["temp_id"]
-		new_command_id = response_message["action_id"]
+		temp_id = response_message['data']["temp_id"]
+		new_command_id = response_message['data']["action_id"]
 
 		if temp_id in self.actions:
 			action = self.actions[temp_id]
@@ -61,29 +61,29 @@ class ActionClientManager():
 			del self.actions[temp_id]
 
 	def receive_feedback(self, response_message : Dict):
-		action_id = response_message["action_id"]
-		feedback = response_message["feedback"]
+		action_id = response_message['data']["action_id"]
+		feedback = response_message['data']["feedback"]
 		if action_id in self.actions:
 			self.actions[action_id].receive_feedback(feedback)
 
 	def receive_result(self, response_message : Dict):
-		action_id = response_message["action_id"]
-		result = response_message["result"]
+		action_id = response_message['data']["action_id"]
+		result = response_message['data']["result"]
 		if action_id in self.actions:
 			self.actions[action_id].receive_result(result)
 
 	def send_cancel_request(self, command_id):
-		data_to_send = {'type': 'cancel_request'}
-		data_to_send['action_id'] = command_id
+		data_to_send = {'type': 'cancel_request', 'data' : {}}
+		data_to_send['data']['action_id'] = command_id
 		self.TCP_Client.write(data_to_send)
 
 	def receive_preempt(self, response_message : Dict):
-		action_id = response_message["action_id"]
+		action_id = response_message['data']["action_id"]
 		if action_id in self.actions:
 			self.actions[action_id].preempted()
 
 	def receive_cancel_response(self, response_message : Dict):
-		action_id = response_message["action_id"]
-		cancelled = response_message["cancelled"]
+		action_id = response_message['data']["action_id"]
+		cancelled = response_message['data']["cancelled"]
 		if action_id in self.actions:
 			self.actions[action_id].receive_cancel_response(cancelled)
