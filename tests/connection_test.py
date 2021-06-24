7b3583155d3c2ef6dbfbedb65a29ee27e6609f96
Victor <victor.bayona@ecl18.ec-lyon.fr>
2021-06-24 14:51:14 +0200

    add test for sending commands

--- tests/connection_test.py
+++ tests/connection_test.py
@@ -9,12 +9,42 @@ class ConnectionTest(unittest.TestCase):
     def test_connect_simulation(self):
 
         # start simulation
-        sim = subprocess.Popen([os.environ["GODOT_PATH"], "--main-pack", " Simulation-Factory-Godot/simu/simulation.pck", "--scenario", os.environ["GITHUB_WORKSPACE"] + "/simu/scenarios/new_scenario_with_environment.json"])
+        sim = subprocess.Popen([os.environ["GODOT_PATH"], "--main-pack", " Simulation-Factory-Godot/simu/simulation.pck",
+         "--scenario", os.environ["GITHUB_WORKSPACE"] + "/simu/scenarios/new_scenario.json",
+         "--environment", os.environ["GITHUB_WORKSPACE"] + "/simu/environments/new_environment.json"])
 
         client = CompleteClient("localhost",10000)
         try:
             #try connecting client
             self.assertTrue(client.wait_for_server(10))
+            client.kill()
+
+        finally:
+            sim.kill()
+            sim.wait()
+
+    def test_send_commands(self):
+
+        sim = subprocess.Popen([os.environ["GODOT_PATH"], "--main-pack", " Simulation-Factory-Godot/simu/simulation.pck",
+         "--scenario", os.environ["GITHUB_WORKSPACE"] + "/simu/scenarios/new_scenario.json",
+         "--environment", os.environ["GITHUB_WORKSPACE"] + "/simu/environments/new_environment.json"])
+        client = CompleteClient("localhost",10000)
+        try:
+            #try connecting client
+            self.assertTrue(client.wait_for_server(10))
+
+            #command that should work
+            action_id = client.ActionClientManager.run_command(['navigate_to','robot0', 15, 15])
+            self.assertTrue(client.ActionClientManager.wait_result(action_id,10))
+
+            #commands that should not work
+            action_id = client.ActionClientManager.run_command(['wrong_command','robot0', 15, 15])
+            self.assertFalse(client.ActionClientManager.wait_result(action_id,10))
+            action_id = client.ActionClientManager.run_command(['navigate_to','wrong_target', 15, 15])
+            self.assertFalse(client.ActionClientManager.wait_result(action_id,10))
+            action_id = client.ActionClientManager.run_command(['navigate_to','robot0']) #wrong_number of arguments
+            self.assertFalse(client.ActionClientManager.wait_result(action_id,10))
+
             client.kill()
 
         finally:
