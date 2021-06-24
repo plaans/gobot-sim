2a38b0d049abec417dfe437672b47b14a672ccf0
Victor <victor.bayona@ecl18.ec-lyon.fr>
2021-06-24 15:18:21 +0200

    add Navigation Tests

---
+++ tests/navigation_tests.py
@@ -0,0 +1,38 @@
+import time
+import subprocess
+import threading 
+import sys
+
+import unittest
+import subprocess
+import os
+
+from ..clients.python_client.CompleteClient import CompleteClient
+
+class NavigationTests(unittest.TestCase):
+
+    def test_navigate_to(self):
+
+        # start simulation
+        sim = subprocess.Popen([os.environ["GODOT_PATH"], "--main-pack", " Simulation-Factory-Godot/simu/simulation.pck",
+         "--scenario", os.environ["GITHUB_WORKSPACE"] + "/simu/scenarios/new_scenario.json",
+         "--environment", os.environ["GITHUB_WORKSPACE"] + "/simu/environments/new_environment.json"])
+
+        client = CompleteClient("localhost",10000)
+        try:
+            #try connecting client
+            self.assertTrue(client.wait_for_server(10))
+
+            action_id = client.ActionClientManager.run_command(['navigate_to','robot0', 15, 15])
+            self.assertTrue(client.ActionClientManager.wait_result(action_id,10))
+
+            final_position = client.StateClient.robot_coordinates('robot0')
+            distance_squared = (final_position[0]-15)**2 + (final_position[1]-15)**2
+            self.assertTrue(distance_squared<=0.1)
+
+            client.kill()
+
+        finally:
+            sim.kill()
+            sim.wait()
+            
