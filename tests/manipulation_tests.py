9975f9f29d15752a3ad2f8ae013a1e0459e2047c
Victor <victor.bayona@ecl18.ec-lyon.fr>
2021-07-06 15:40:38 +0200

    improve readability of tests

--- tests/manipulation_tests.py
+++ tests/manipulation_tests.py
@@ -9,26 +9,31 @@ class ManipulationTests(SimulationTestBase):
         while self.client.StateClient.packages_list() == None:
             time.sleep(0.1)
 
+        #get a package in the list of existing packages
         self.target_package = self.client.StateClient.packages_list()[0]
 
+        #find the parent node of the package and wait until it is a belt (which means the package is ready to pickup)
         package_parent_node = self.client.StateClient.package_location(self.target_package)
         while package_parent_node == None or self.client.StateClient.instance_type(package_parent_node) != 'Belt.instance':
             package_parent_node = self.client.StateClient.package_location(self.target_package)
             time.sleep(0.1)
 
+        #get an interact area of the belt
         interact_area = self.client.StateClient.belt_interact_areas(package_parent_node)[0]
 
+        #make the robot navigate to this interact area, then face the belt
         action_id = self.client.ActionClientManager.run_command(['navigate_to_area','robot0', interact_area])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, 10))
-        
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
         action_id = self.client.ActionClientManager.run_command(['face_belt', 'robot0',package_parent_node, 5])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, 10))
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
 
+        #make the robot pick
         action_id = self.client.ActionClientManager.run_command(['pick','robot0'])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, 10))
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
 
         time.sleep(0.5)
 
+        #check the package supposed to have been picked (first of the belt) has now the robot as location
         self.assertEqual(self.client.StateClient.package_location(self.target_package), 'robot0')
 
     def test_pick_package(self):
@@ -46,13 +51,13 @@ class ManipulationTests(SimulationTestBase):
         interact_area = self.client.StateClient.belt_interact_areas(belt)[0]
 
         action_id = self.client.ActionClientManager.run_command(['navigate_to_area','robot0', interact_area])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, 10))
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
         
         action_id = self.client.ActionClientManager.run_command(['face_belt', 'robot0',belt, 5])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, 10))
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
 
         action_id = self.client.ActionClientManager.run_command(['pick_package','robot0', package_to_pick])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, 10))
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
 
         time.sleep(0.5)
 
@@ -60,10 +65,11 @@ class ManipulationTests(SimulationTestBase):
 
 
     def test_place(self):
+        #first reuse previous pick test to end up with the robot carrying a package
         self.test_pick()
 
         #now that package was picked find where to place it
-        
+        #for that take the first process in the list of processes to be done, and search for a machine that can do this process
         process_to_do = self.client.StateClient.package_processes_list(self.target_package)[0][0]
         machine_chosen = None
         machines_list = self.client.StateClient.machines_list()
@@ -73,19 +79,22 @@ class ManipulationTests(SimulationTestBase):
                 machine_chosen = machine
                 break
         
+        #then find the input_belt of this machine and procede as for pick test : find the interact area, navigate to it and face the belt
         belt = self.client.StateClient.machine_input_belt(machine_chosen)
         interact_area = self.client.StateClient.belt_interact_areas(belt)[0]
 
         action_id = self.client.ActionClientManager.run_command(['navigate_to_area','robot0', interact_area])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, 10))
-        
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
         action_id = self.client.ActionClientManager.run_command(['face_belt', 'robot0',belt, 5])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, 10))
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
 
+        #then send place command
         action_id = self.client.ActionClientManager.run_command(['place','robot0'])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, 10))
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
 
         time.sleep(0.5)
+
+        #check the package is now located in the machine it is supposed to be in
         self.assertEqual(self.client.StateClient.package_location(self.target_package), machine_chosen)
 
 if __name__ == '__main__':
--- tests/rotation_tests.py
+++ tests/rotation_tests.py
@@ -6,25 +6,37 @@ from .SimulationTestBase import SimulationTestBase
 class RotationTests(SimulationTestBase):
 
     def test_do_rotation(self):
+        #get the start rotation which will be used to compute the expected final rotation
         current_rotation = self.client.StateClient.robot_rotation('robot0')
         while current_rotation == None:
             current_rotation = self.client.StateClient.robot_rotation('robot0')
             time.sleep(0.1)
              
-        action_id = self.client.ActionClientManager.run_command(['do_rotation','robot0', -0.3, 1.5])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id,10))
+        speed = -0.3
+        duration = 1.5
+        
+        action_id = self.client.ActionClientManager.run_command(['do_rotation','robot0', speed, duration])
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
 
-        self.assertAlmostEqual(self.client.StateClient.robot_rotation('robot0'), current_rotation - 0.3*1.5, delta=0.01)
+        #check final rotation is closed enough to rotation expected
+        self.assertAlmostEqual(self.client.StateClient.robot_rotation('robot0'), current_rotation + speed*duration, delta=0.01)
 
     def test_rotate_to(self):
-        action_id = self.client.ActionClientManager.run_command(['rotate_to','robot0', 1.5, 0.5])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id,10))
+        angle = 1.5
+        speed = 0.5
 
-        self.assertAlmostEqual(self.client.StateClient.robot_rotation('robot0'), 1.5, delta=0.01)
+        action_id = self.client.ActionClientManager.run_command(['rotate_to','robot0', angle, speed])
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
+
+        #check final rotation is closed enough to rotation expected
+        self.assertAlmostEqual(self.client.StateClient.robot_rotation('robot0'), angle, delta=0.01)
 
     def test_face_belt(self):
-        action_id = self.client.ActionClientManager.run_command(['face_belt','robot0', 'belt0', 0.5])
-        self.assertTrue(self.client.ActionClientManager.wait_result(action_id,10))
+        speed = 0.5
+
+        #make the robot face the belt and check the command ended successfully
+        action_id = self.client.ActionClientManager.run_command(['face_belt','robot0', 'belt0', speed])
+        self.assertTrue(self.client.ActionClientManager.wait_result(action_id, timeout=10))
             
 if __name__ == '__main__':
     unittest.main()
